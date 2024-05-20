37932244b7adf022ac51335c1e71a6d442f48ea4
John Thomas <netflixemailaccount@gmail.com>
2024-05-20 08:38:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,17 @@ from PIL import Image
 def create_map():
     return folium.Map(location=[20, 0], zoom_start=4)
 
+
+def plot_pretty_map(lat, lon):
+    try:
+        aoi = get_aoi(address=f"{lat},{lon}", radius=1000, rectangular=False)
+        df = get_osm_geometries(aoi=aoi)
+        fig = Plot(df=df, aoi_bounds=aoi.bounds, draw_settings=STYLES["Peach"]).plot_all()
+        return fig
+    except Exception as e:
+        st.error(f"Error: {e}")
+        return None
+
+
+
+
